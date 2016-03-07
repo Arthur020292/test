2035867adf904e4a75138f4a227e7f6ffaa2c297
Arthur Baduyen <arthur020292@gmail.com>
2016-03-06 18:52:43 -0800

    Added files via upload

---
+++ myscript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+	$("button").click(function(){
+		//$("button").text($('input').val())
+		//$(".modify").css("width","500px");
+		$("div").removeClass("modify");
+		$("div").addClass("addme");
+		});
+
+	});
+});
---
+++ script.js
@@ -0,0 +1,73 @@
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#myheader-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#myheader').addClass('stick');
+        $('#myheader-anchor').height($('#myheader').outerHeight());
+    } else {
+        $('#myheader').removeClass('stick');
+        $('#myheader-anchor').height(0);
+    }
+}
+
+
+
+
+(function() {
+  var delay = false;
+
+  $(document).on('mousewheel DOMMouseScroll', function(event) {
+    event.preventDefault();
+    if(delay) return;
+
+    delay = true;
+    setTimeout(function(){delay = false},200)
+
+    var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+
+    var a= document.getElementsByTagName('a');
+    if(wd < 0) {
+      for(var i = 0 ; i < a.length ; i++) {
+        var t = a[i].getClientRects()[0].top;
+        if(t >= 40) break;
+      }
+    }
+    else {
+      for(var i = a.length-1 ; i >= 0 ; i--) {
+        var t = a[i].getClientRects()[0].top;
+        if(t < -20) break;
+      }
+    }
+    $('html,body').animate({
+      scrollTop: a[i].offsetTop
+    });
+  });
+
+
+
+
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+
+
+
+})();
+
+
+
+var dir = 1;
+var MIN_TOP = 200;
+var MAX_TOP = 350;
+
+function autoscroll() {
+    var window_top = $(window).scrollTop() + dir;
+    if (window_top >= MAX_TOP) {
+        window_top = MAX_TOP;
+        dir = -1;
+    } else if (window_top <= MIN_TOP) {
+        window_top = MIN_TOP;
+        dir = 1;
+    }
+    $(window).scrollTop(window_top);
+    window.setTimeout(autoscroll, 100);
+}
